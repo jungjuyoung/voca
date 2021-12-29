7040cc25d9417253a41620fa3a36f0c295ca13ae
jungjuyoung <nwwn1004@naver.com>
2021-12-30 00:45:19 +0900

    feat: mongoDB connect

--- back/config/default.ts
+++ back/config/default.ts
@@ -1,4 +1,6 @@
 export default {
   port: 3005,
-  dbUri: "",
+  dbUri:
+    "mongodb+srv://nadia:1q2w3e4r@cluster0.fuxot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  saltWorkFactor: 10,
 };
--- back/src/app.ts
+++ back/src/app.ts
@@ -1,9 +1,14 @@
-import express from "express";
+import express, { Express } from "express";
 import config from "config";
+import connect from "./utils/connect";
+import logger from "./utils/logger";
+import routes from "./routes";
 
-const app = express();
+const app: Express = express();
 const port = config.get<number>("port");
 
-app.listen(port, () => {
-  console.log(`Express server listning on port:${port}`);
+app.listen(port, async () => {
+  logger.info(`Express server listning on http://localhost:${port}`);
+  routes(app);
+  connect();
 });
